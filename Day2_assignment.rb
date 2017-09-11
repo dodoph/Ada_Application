3f3c70ddb046125eeb1bd323da27d72eb89cc5f0
Jessie Tang <tangqiaojie@hotmail.com>
2017-09-11 12:16:36 -0700

    Day3 lesson

--- Day2_assignment.rb
+++ Day2_assignment.rb
@@ -7,21 +7,27 @@
 #>> -4
 #>> 2
 
-if nil
-  puts "uu"
-  end
-
-x = true
-y = false
-if x && y
-  puts "1"
-end
-
-if x || y
-  puts "2"
-end
-
-x = 1
-if x == false || x == 1 || x == 48
-	puts "x is false, 2, or 48"
+# if nil
+#   puts "uu"
+#   end
+#
+# x = true
+# y = false
+# if x && y
+#   puts "1"
+# end
+#
+# if x || y
+#   puts "2"
+# end
+#
+# x = 1
+# if x == false || x == 1 || x == 48
+# 	puts "x is false, 2, or 48"
+# end
+#
+i = 0
+until i == 4
+   puts i
+   i += 1
 end
