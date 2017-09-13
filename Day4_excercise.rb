6e7a6ba8249a27d2bda8d87bf1c3e96ed4d0dd97
Jessie Tang <tangqiaojie@hotmail.com>
2017-09-13 14:17:25 -0700

    Day 4 excercise check

--- Day4_excercise.rb
+++ Day4_excercise.rb
@@ -1,9 +1,14 @@
 print "Please create your password: "
 password = gets.chomp
 numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-# characters = ["a","b","c","d","e", "f", "g", "h", "i"]
-characters = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ".chr
+symbols = ["@", "%", "*", "!"]
+uppercharacters = ("A".."Z").to_a
+lowercharacters = ("a".."z").to_a
 hasNumber = false
+hasSymbol = false
+hasUpper = false
+hasLower = false
+hasMoreThan8Characters = false
 password.each_char do |element| #switch string to chars
   numbers.each do |number|
     if element == number
@@ -14,16 +19,50 @@ password.each_char do |element| #switch string to chars
   # if hasNumber == true
   #   break
   # end
-  characters.each_char do |character|
-    if element == character
-      hasNumber = true
+  symbols.each do |symbol|
+    if symbol == element
+      hasSymbol = true
+      break
+    end
+  end
+  uppercharacters.each do |uppercharacter|
+    if element == uppercharacter
+      hasUpper = true
+      break
+    end
+  end
+  lowercharacters.each do |lowercharacter|
+    if element == lowercharacter
+      hasLower = true
       break
     end
   end
 end
 
-if hasNumber
-  puts "has number"
+if hasNumber == false
+  puts "Enter at least one number"
+end
+
+if hasSymbol == false
+  puts "Enter at least one symbol"
+end
+
+if hasUpper == false
+  puts "Enter at least one uppercharacter"
+end
+
+if hasLower == false
+  puts "Enter at least one lowercharacter"
+end
+
+if password.length >= 8
+  hasMoreThan8Characters = true
 else
-  puts "does not have number"
+  puts "Enter at least 8 characters"
+end
+
+if hasNumber && hasSymbol && hasLower && hasUpper && hasMoreThan8Characters
+  puts "Yes, we created your password"
+else
+  puts "Please do it again"
 end
